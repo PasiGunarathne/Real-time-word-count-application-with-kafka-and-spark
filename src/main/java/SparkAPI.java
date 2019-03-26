e29e5155393d7e83c827c13ec470ae908d7a081a
PasiGunarathne <pasigunarathne@gmail.com>
2019-03-26 23:19:56 +0530

    create a java file

---
+++ src/main/java/SparkAPI.java
@@ -0,0 +1,32 @@
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * Created by pasindu on 4/7/19.
+ */
+public class SparkAPI {
+
+    public static void main(String[] args) {
+        System.out.println("hello java spark");
+
+
+
+    }
+
+
+    public void wordCount(){
+
+    }
+
+
+
+}
