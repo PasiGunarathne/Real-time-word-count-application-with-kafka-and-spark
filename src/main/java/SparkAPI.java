080f311ad5e8e506dbb2359988f7254b5c12f472
PasiGunarathne <pasigunarathne@gmail.com>
2019-03-26 23:21:54 +0530

    added main method and other files

--- src/main/java/SparkAPI.java
+++ src/main/java/SparkAPI.java
@@ -1,11 +1,11 @@
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Produced;
+//import org.apache.kafka.common.serialization.Serdes;
+//import org.apache.kafka.streams.KafkaStreams;
+//import org.apache.kafka.streams.StreamsBuilder;
+//import org.apache.kafka.streams.StreamsConfig;
+//import org.apache.kafka.streams.Topology;
+//import org.apache.kafka.streams.kstream.KStream;
+//import org.apache.kafka.streams.kstream.KTable;
+//import org.apache.kafka.streams.kstream.Produced;
 
 import java.util.Arrays;
 import java.util.Properties;
@@ -17,6 +17,8 @@ public class SparkAPI {
 
     public static void main(String[] args) {
         System.out.println("hello java spark");
+
+
 
 
 
