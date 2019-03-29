f034448b7f2481b2cdcafac4aa8223938da3ddb4
PasiGunarathne <pasigunarathne@gmail.com>
2019-03-29 23:30:26 +0530

    implement kafka-spark to listen kafka streams and process the count of words

--- src/main/java/SparkAPI.java
+++ src/main/java/SparkAPI.java
@@ -1,11 +1,11 @@
-//import org.apache.kafka.common.serialization.Serdes;
-//import org.apache.kafka.streams.KafkaStreams;
-//import org.apache.kafka.streams.StreamsBuilder;
-//import org.apache.kafka.streams.StreamsConfig;
-//import org.apache.kafka.streams.Topology;
-//import org.apache.kafka.streams.kstream.KStream;
-//import org.apache.kafka.streams.kstream.KTable;
-//import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
 
 import java.util.Arrays;
 import java.util.Properties;
@@ -18,7 +18,23 @@ public class SparkAPI {
     public static void main(String[] args) {
         System.out.println("hello java spark");
 
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+//        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> wordCountInput = builder.stream("word-count-input");
+        KTable<String, Long> wordCounts = wordCountInput
+                .mapValues(textLine -> textLine.toLowerCase())
+                .flatMapValues(lowerCasedTextLine -> Arrays.asList(lowerCasedTextLine.split(" ")))
+                .selectKey((ignoredKey, word) -> word)
+                .groupByKey()
+                .count();
+
+        wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
 
 
 
